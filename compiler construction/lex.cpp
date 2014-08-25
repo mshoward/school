277493353e6ada950be71ac8bd633484b5065874
mshoward <mshoward42@gmail.com>
2014-08-25 17:18:39 -0500

    sfasdf

--- compiler construction/lex.cpp
+++ compiler construction/lex.cpp
@@ -1,2 +1,74 @@
 #pragma once
 
+
+using std::string;
+using std::fstream;
+using std::vector;
+
+token::token()
+{
+	val = "";
+	token_class = 0;
+	token_subscript = 0;
+}
+
+token::~token()
+{
+	val.clear();
+}
+
+lex::lex()
+{
+	//fund
+	curr_char = 0;
+	current_token = 0;
+	
+	//stl
+	//nothing to do
+	
+	//custom
+	//nothing to do
+}
+
+lex::~lex()
+{
+	file_stream.sync();
+	file_stream.flush();
+	file_stream.close();
+	token_list.clear();
+	id_table.clear();
+}
+
+bool lex::isDelimiter(char c)
+{
+	
+}
+
+bool lex::isDelimiter(token c)
+{
+}
+
+bool lex::isConstant(token c);
+{
+}
+
+bool lex::isKeyWord(char c)
+{
+	return
+	(
+		(c == ';') ||
+		(c == ',') ||
+		(c == '+') ||
+		(c == '*') ||
+		(c == '-') ||
+		(c == '/') ||
+		(c == ':')
+	);
+}
+
+bool lex::isKeyWord(token c)
+{
+}
+
+
+
--- compiler construction/lex.hpp
+++ compiler construction/lex.hpp
@@ -1,13 +1,45 @@
 #pragma once
 
+
+using std::string;
+using std::fstream;
+using std::vector;
+
 class token
 {
+public:
 	std::string val;
 	int token_class;
-	int token_subs;
+	int token_subscript;
+	
+	token();
+	~token();
+	
 };
 
 class lex
 {
+public:
+	//fundamental data types
+	char curr_char;
+	unsigned int current_token;
+	
+	//stl data structs
+	std::fstream file_stream;
 	std::vector<token> token_list;
+	std::vector<token> id_table;
+	//custom data structs
+	token curr_token;
+	
+	//constructor
+	lex();
+	~lex();
+	
+	
+	bool isDelimiter(char c); 
+	bool isDelimiter(token c);
+	
+	bool isConstant(token c);
+	bool isKeyWord(char c);
+	bool isKeyWord(token c);
 };
